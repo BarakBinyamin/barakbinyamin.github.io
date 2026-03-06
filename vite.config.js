9dcafce8a42988dcfe968ea8f0fb07af90a5c328
Barak Binaymin <barak.binyamin555@gmail.com>
2026-03-06 02:08:49 -0500

    Addded auto copy 404.html

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-   build: {
+  plugins: [
+    vue(), 
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, 'index.html'),
+          dest: '', 
+          rename: '404.html' 
+        },
+      ],
+    })
+  ],
+  build: {
     outDir: 'docs', 
     emptyOutDir: true, 
   },
