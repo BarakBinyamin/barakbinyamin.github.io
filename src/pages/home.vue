215a290367cab727f11ff2c826281a27d64fb1ab
Barak Binaymin <barak.binyamin555@gmail.com>
2026-03-06 02:38:06 -0500

    padding

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,9 @@
 <template>
-  <div style="position: relative; grid-template-rows: 90% 10%; display: grid; align-items: center; justify-items: center; overflow: hidden; padding: 10px; height: 100vh; width: 100%; ">
+  <div style="position: relative; grid-template-rows: 90% 10%; display: grid; align-items: center; justify-items: center;  padding: 10px; height: 100vh; width: 100%; ">
     <img style="max-height:70%;" type="image/png" src="/logo.png" />
     <div style="margin:0;padding: 5px;text-align: center;">Website in development, please come back soon!</div>
   </div>
+  <div style="margin:0;padding: 5px;text-align: center;"></div>
 </template>
 
 <script>
