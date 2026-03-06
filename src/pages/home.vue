4676361083a396d2cc58458542f40ee96ab49799
Barak Binaymin <barak.binyamin555@gmail.com>
2026-03-06 02:36:29 -0500

    sizing

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,12 +1,9 @@
 <template>
-  <div style="position: relative; display: grid; align-items: center; justify-items: center; overflow: hidden; padding: 10px; height: 100vh; width: 100%; ">
-    <img style="position: relative; width: 40%;"type="image/png" src="/logo.png" />
+  <div style="position: relative; grid-template-rows: 90% 10%; display: grid; align-items: center; justify-items: center; overflow: hidden; padding: 10px; height: 100vh; width: 100%; ">
+    <img style="max-height:70%;" type="image/png" src="/logo.png" />
     <div style="margin:0;padding: 5px;text-align: center;">Website in development, please come back soon!</div>
   </div>
 </template>
 
 <script>
 </script>
-
-<style scoped>
-</style>
