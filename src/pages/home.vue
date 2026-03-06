8414593a04f02a166f03f85dbffb1dadcf5a00ed
Barak Binaymin <barak.binyamin555@gmail.com>
2026-03-06 02:38:55 -0500

    overflow

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="position: relative; grid-template-rows: 90% 10%; display: grid; align-items: center; justify-items: center;  padding: 10px; height: 100vh; width: 100%; ">
+  <div style="position: relative; grid-template-rows: 90% 10%; display: grid; align-items: center; overflow: hidden; justify-items: center;  padding: 10px; height: 100vh; width: 100%; ">
     <img style="max-height:70%;" type="image/png" src="/logo.png" />
     <div style="margin:0;padding: 5px;text-align: center;">Website in development, please come back soon!</div>
   </div>
